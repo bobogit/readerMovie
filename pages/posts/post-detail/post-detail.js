a9f7091c8574a8c95200a239bc5befdd2f637034
wangbo <wangbo@myttpl.com>
2019-03-31 23:16:28 +0800

    init

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -14,16 +14,55 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var postId = options.id;
+
+    this.data.postId = postId;
+
     var postData = postsData.postList[postId];
     this.setData(postData);
+
+    /*
+      var postsCollected = {
+        1: "true",
+        2: "false",
+        3: "true",
+        ...
+      }
+
+    */
+
+    var postsCollected = wx.getStorageSync('posts_collected');
+    if (postsCollected) {
+      var postCollected = postsCollected[postId];
+      if(postCollected) {
+        this.setData({
+          collected: postCollected
+        })
+      }
+      
+    }else{
+      postsCollected = {};
+      postsCollected[postId] = false;
+      wx.setStorageSync('posts_collected', postsCollected);
+    }
+
   },
 
   onCollectionTap: function(event) {
-    var game = wx.getStorageSync('key');
-    console.log(game)
+    var postsCollected = wx.getStorageSync('posts_collected');
+
+    var postId = this.data.postId;
+
+    var postCollected = postsCollected[postId];
+    postCollected = !postCollected;
+    postsCollected[postId] = postCollected;
+    wx.setStorageSync('posts_collected', postsCollected);
+
+    this.setData({
+      collected: postCollected
+    })
   }
 
-  
+
 })
