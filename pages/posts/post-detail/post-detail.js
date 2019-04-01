57af4e4bf082842402da875bb0d9f1c54530b5de
wangbo <wangbo@myttpl.com>
2019-04-01 18:02:11 +0800

    init

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -8,7 +8,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isPlayingMusic : false
   },
 
   /**
@@ -35,17 +35,36 @@ Page({
     var postsCollected = wx.getStorageSync('posts_collected');
     if (postsCollected) {
       var postCollected = postsCollected[postId];
-      if(postCollected) {
+      if (postCollected) {
         this.setData({
           collected: postCollected
         })
       }
-      
-    }else{
+
+    } else {
       postsCollected = {};
       postsCollected[postId] = false;
       wx.setStorageSync('posts_collected', postsCollected);
     }
+
+    var that = this;
+    wx.onBackgroundAudioPlay(function(){
+      that.setData({
+        isPlayingMusic: true
+      })
+    });
+
+    wx.onBackgroundAudioPause(function(){
+      that.setData({
+        isPlayingMusic: false
+      })
+    });
+
+    wx.onBackgroundAudioStop(function(){
+      that.setData({
+        isPlayingMusic: false
+      })
+    });
 
   },
 
@@ -62,6 +81,56 @@ Page({
     this.setData({
       collected: postCollected
     })
+
+    wx.showToast({
+      title: postCollected ? '收藏成功' : '取消成功',
+      duration: 2000
+    })
+
+  },
+
+  onShareTap: function(event) {
+
+    var itemList = [
+      "分享给微信好友",
+      "分享到朋友圈",
+      "分享到QQ",
+      "分享到微博"
+    ];
+
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor: "#405F80",
+      success: function(res) {
+        //res.tabIndex 数组元素的序号,从0开始
+        //res.cancel 用户是否点击了取消按钮
+        wx.showModal({
+          title: '用户分享到了' + itemList[res.tapIndex],
+          content: '现在无法实现分享功能',
+        })
+      }
+    })
+  },
+
+  onMusicTap: function(event){
+
+    var isPlayingMusic = this.data.isPlayingMusic;
+  
+    if(isPlayingMusic) {
+      wx.pauseBackgroundAudio();
+
+    }else{
+      wx.playBackgroundAudio({
+        dataUrl: this.data.music.url,
+        title: this.data.music.title,
+        coverImgUrl: this.data.music.coverImg
+      })
+    }
+  
+    this.setData({
+      isPlayingMusic: !isPlayingMusic
+    })
+   
   }
 
 
