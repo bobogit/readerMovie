98e91b473bcea9e9b05eae2628b3b70af3ddc160
wangbo <804623282@qq.com>
2019-04-10 17:12:06 +0800

    init

--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -1,7 +1,7 @@
-// pages/movies/movie-detail/movie-detail.js
-
+import { Movie } from 'class/Movie.js';
 var app = getApp();
-var util = require('../../../utils/util.js');
+// var util = require('../../../utils/util.js');
+
 
 Page({
 
@@ -17,50 +17,63 @@ Page({
    */
   onLoad: function (options) {
     var url = app.globalData.doubanBase + '/v2/movie/subject/' + options.idx;
-    util.http(url, this.processDoubanData);
-  },
+    var movie = new Movie(url);
+    var that = this;
+    // movie.getMovieData(function (movie){
+    //   that.setData({
+    //     movie: movie
+    //   })
+    // })
+    // util.http(url, this.processDoubanData);
 
-  processDoubanData: function(data) {
-
-      if(!data){
-        return;
-      }
-
-      var director = {
-        avatar : "",
-        name: "",
-        id: ""
-      }
-
-      if(data.directors[0] != null) {
-        if(data.directors[0].avatars != null) {
-          director.avatar = data.directors[0].avatars.large;
-        }
-        director.name = data.directors[0].name;
-        director.id = data.directors[0].id;
-      }
-
-      var movie = {
-        movieImg: data.images ? data.images.large : "",
-        country: data.countries[0],
-        title: data.title,
-        originalTitle: data.original_title,
-        wishCount: data.wish_count,
-        commentsCount: data.comments_count,
-        year: data.year,
-        genres: data.genres.join(","),
-        stars : util.convertToStarArray(data.rating.stars),
-        score: data.rating.average,
-        director: director,
-        casts: util.convertToCastString(data.casts),
-        castsInfo: util.convertToCastInfos(data.casts),
-        summary: data.summary
-      }
-
+    movie.getMovieData((movie) => {
       this.setData({
         movie: movie
       })
+    })
   }
+
+  // processDoubanData: function(data) {
+
+  //     if(!data){
+  //       return;
+  //     }
+
+  //     var director = {
+  //       avatar : "",
+  //       name: "",
+  //       id: ""
+  //     }
+
+  //     if(data.directors[0] != null) {
+  //       if(data.directors[0].avatars != null) {
+  //         director.avatar = data.directors[0].avatars.large;
+  //       }
+  //       director.name = data.directors[0].name;
+  //       director.id = data.directors[0].id;
+  //     }
+
+  //     var movie = {
+  //       movieImg: data.images ? data.images.large : "",
+  //       country: data.countries[0],
+  //       title: data.title,
+  //       originalTitle: data.original_title,
+  //       wishCount: data.wish_count,
+  //       commentsCount: data.comments_count,
+  //       year: data.year,
+  //       genres: data.genres.join(","),
+  //       stars : util.convertToStarArray(data.rating.stars),
+  //       score: data.rating.average,
+  //       director: director,
+  //       casts: util.convertToCastString(data.casts),
+  //       castsInfo: util.convertToCastInfos(data.casts),
+  //       summary: data.summary
+  //     }
+
+  //     this.setData({
+  //       movie: movie
+  //     })
+  // }
 
   
 })
